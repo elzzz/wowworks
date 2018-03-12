6af82b4a0c5887ab0b1724d23c4a46794f663f97
Klim Lyapin <westn23@mail.ru>
2018-03-12 18:23:35 +0400

    added working docker-compose file

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'l2JG-K7BsnAngrGaxeT6vvUizvNxsZ9V',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
