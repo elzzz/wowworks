a45e96d4e2da2cf1a6bfbbd3892830cf7a596956
Klim Lyapin <westn23@mail.ru>
2018-03-12 19:14:51 +0400

    configured app

--- config/db.php
+++ config/db.php
@@ -2,10 +2,16 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host=pg;dbname=ww_dev',
+    'username' => 'ww_user',
+    'password' => 'ww_pw',
     'charset' => 'utf8',
+    'schemaMap' => [
+        'pgsql' => [
+            'class' => 'yii\db\pgsql\Schema',
+            'defaultSchema' => 'public'
+        ]
+    ],
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
